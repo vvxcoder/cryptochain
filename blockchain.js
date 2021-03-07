11f336155e29527e858f34a74bd9f3cb1c613e56
Valerii Voronkov <scorpirat@gmail.com>
2021-03-07 11:40:07 +0300

    Add Blockchain class.

---
+++ blockchain.js
@@ -0,0 +1,19 @@
+const Block = require('./block');
+
+class Blockchain {
+    constructor() {
+        this.chain = [Block.genesis()];
+    }
+
+    addBlock({ data }) {
+        const newBlock = Block.mineBlock({
+            lastBlock: this.chain[this.chain.length-1],
+            data
+        });
+
+        this.chain = [...this.chain, newBlock];
+    }
+
+}
+
+module.exports = Blockchain;
