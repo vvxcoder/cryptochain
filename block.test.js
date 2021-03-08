2b4da1cbed07a3827bd7fdc57ecd3af3fee888bc
Valerii Voronkov <scorpirat@gmail.com>
2021-03-08 20:23:49 +0300

    Add difficulty and the nonce value tests.

--- block.test.js
+++ block.test.js
@@ -7,12 +7,16 @@ describe('Block', () => {
     const lastHash = 'foo-hash';
     const hash = 'bar-hash';
     const data = ['blockchain', 'data'];
+    const nonce = 1;
+    const difficulty = 1;
 
     const block = new Block({
         timestamp,
         lastHash,
         hash,
         data,
+        nonce,
+        difficulty
     });
 
     it('has a timestamp, lastHash, hash and data properties', () => {
@@ -20,6 +24,8 @@ describe('Block', () => {
         expect(block.lastHash).toEqual(lastHash);
         expect(block.hash).toEqual(hash);
         expect(block.data).toEqual(data);
+        expect(block.nonce).toEqual(nonce);
+        expect(block.difficulty).toEqual(difficulty);
     });
 });
 
@@ -57,6 +63,11 @@ describe('mineBlock()', () => {
     });
 
     it('creates a SHA-256 `hash` based on the proper inputs', () => {
-        expect(minedBlock.hash).toEqual(cryptoHash(minedBlock.timestamp, lastBlock.hash, data));
+        expect(minedBlock.hash).toEqual(cryptoHash(minedBlock.timestamp, 
+            minedBlock.nonce, minedBlock.difficulty, lastBlock.hash, data));
+    });
+
+    it('sets a `hash` that matches the difficulty criteria', () => {
+        expect(minedBlock.hash.substring(0, minedBlock.difficulty)).toEqual('0'.repeat(minedBlock.difficulty));
     });
 });
