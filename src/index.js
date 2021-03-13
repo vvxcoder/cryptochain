44d1210946ea1d5d9e989d31c43441ea003b9a6a
Valerii Voronkov <scorpirat@gmail.com>
2021-03-13 23:36:15 +0300

    Post request to mine a block.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const Blockchain = require('../src/blockchain');
 const app = express();
 const blockchain = new Blockchain();
 
+app.use(bodyParser.json());
+
 app.get('/api/blocks', (req, resp) => {
     resp.json(blockchain.chain);
+});
+
+app.post('/api/mine', (req, resp) => {
+    const { data } = req.body;
+    blockchain.addBlock({ data });
+    resp.redirect('/api/blocks');
 });
 
 const PORT = 3099;
