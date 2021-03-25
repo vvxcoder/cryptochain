d4b55832eed793dfe8a3822e1f2be75c70caa5d8
Valerii Voronkov <scorpirat@gmail.com>
2021-03-25 22:06:57 +0300

    Add sync chain on connect.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const request = require('request');
 const bodyParser = require('body-parser');
 const Blockchain = require('../src/blockchain');
 const PubSub = require('../src/publish');
@@ -6,6 +7,9 @@ const PubSub = require('../src/publish');
 const app = express();
 const blockchain = new Blockchain();
 const pubsub = new PubSub({ blockchain });
+
+const DEFAULT_PORT = 3099;
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 
 setTimeout(() => {
     pubsub.broadcastChain();
@@ -24,8 +28,16 @@ app.post('/api/mine', (req, resp) => {
     resp.redirect('/api/blocks');
 });
 
+const syncChains = () => {
+    request({ url: `${ROOT_NODE_ADDRESS}/api/blocks` }, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+            const rootChain = JSON.parse(body);
+            console.log('Replce chain on a sync with = ', rootChain);
+            blockchain.replaceChain(rootChain);
+        }
+    });
+};
 
-const DEFAULT_PORT = 3099;
 let PEER_PORT;
 
 if (process.env.GENERATE_PEER_PORT === 'true') {
@@ -36,4 +48,6 @@ const PORT = PEER_PORT || DEFAULT_PORT;
 
 app.listen(PORT, () => {
     console.log(`Listening at localhost:${PORT}`);
+
+    syncChains();
 });
