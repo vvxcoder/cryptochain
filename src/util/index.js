6f49d7f8330f4cd0a64468cf8365c270fbc037ac
Valerii Voronkov <scorpirat@gmail.com>
2021-03-28 16:52:47 +0300

    Sign data and verifying signatures.

--- src/util/index.js
+++ src/util/index.js
@@ -1,5 +1,12 @@
 const EC = require('elliptic').ec;
-
 const ec = new EC('secp256k1');
+const cryptoHash = require('../util/crypto-hash');
 
-module.exports = { ec };
+const verifySignature = ({
+    publicKey, data, signature
+}) => {
+    const keyFromPublic = ec.keyFromPublic(publicKey, 'hex');
+    return keyFromPublic.verify(cryptoHash(data), signature);
+};
+
+module.exports = { ec, verifySignature };
