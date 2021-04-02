8b944d074470c20024ac052fbd72cf9f7011faa2
Valerii Voronkov <scorpirat@gmail.com>
2021-04-02 21:44:08 +0300

    Add transaction miner class.

---
+++ src/app/transaction-miner.js
@@ -0,0 +1,23 @@
+class TransactionMiner {
+    mineTransactions() {
+        constructor({
+            blockchain, transactionPool, wallet, pubsub
+        }) {
+            this.blockchain = blockchain;
+            this.transactionPool = transactionPool;
+            this.wallet = wallet;
+            this.pubsub = pubsub;
+        }
+        //get the transaction pool's valid transactions
+
+        //generate the miner's reward
+
+        //add a block consisting of these transactions to the blockchain
+
+        //broadcast the updated blockchain
+
+        //clear the pool
+    }
+}
+
+module.exports = TransactionMiner;
