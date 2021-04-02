fef348ebd0373e7c4129d6d8e89bd54cc7e5edfa
Valerii Voronkov <scorpirat@gmail.com>
2021-04-02 21:12:41 +0300

    Sync transaction pool map on connect.

--- src/index.js
+++ src/index.js
@@ -60,12 +60,20 @@ app.get('/api/transaction-pool-map', (req, resp) => {
     resp.json(transactionPool.transactionMap);
 });
 
-const syncChains = () => {
+const syncWithRootState = () => {
     request({ url: `${ROOT_NODE_ADDRESS}/api/blocks` }, (error, response, body) => {
         if (!error && response.statusCode === 200) {
             const rootChain = JSON.parse(body);
             console.log('Replce chain on a sync with = ', rootChain);
             blockchain.replaceChain(rootChain);
+        }
+    });
+
+    request({ url: `${ROOT_NODE_ADDRESS}/api/transaction-pool-map` }, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+            const rootTransactionPoolMap = JSON.parse(body);
+            console.log('replace transaction pool map on a sync with', rootTransactionPoolMap);
+            transactionPool.setMap(rootTransactionPoolMap);
         }
     });
 };
@@ -82,6 +90,6 @@ app.listen(PORT, () => {
     console.log(`Listening at localhost:${PORT}`);
 
     if (PORT !== DEFAULT_PORT) {
-        syncChains();
+        syncWithRootState();
     }
 });
